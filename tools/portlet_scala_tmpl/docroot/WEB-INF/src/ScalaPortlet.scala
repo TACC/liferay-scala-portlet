6dd8c1e1d3638cf2c851ab82fc9763a1d47fdbff
David Choi <choi.david.e@gmail.com>
2012-08-22 09:24:05 -0500

    scala

---
+++ tools/portlet_scala_tmpl/docroot/WEB-INF/src/ScalaPortlet.scala
@@ -0,0 +1,16 @@
+import edu.utexas.tacc.portlet.AbstractPortlet
+import javax.portlet._
+
+class ScalaPortlet extends AbstractPortlet {
+  override def doView(req: RenderRequest, resp: RenderResponse) {
+
+  }
+  
+  override def processAction(req: ActionRequest, resp: ActionResponse) {
+
+  }
+
+  override def serveResource(req: ResourceRequest, resp: ResourceResponse) {
+
+  }
+}
